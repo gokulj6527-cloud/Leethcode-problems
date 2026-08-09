9fea3702acd64971377cb9f8ea5efa03b9bdfb57
Gokul <gokulj6527@gmail.com>
2026-08-09 11:05:02 +0530

    Time: 91 ms (5.92%) | Memory: 142.7 MB (7.23%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) 
+    {
+        Stack<Integer> stack = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < temperatures.length; i++) 
+        {
+            while (!stack.isEmpty() &&
+                   temperatures[i] > temperatures[stack.peek()]) 
+            {
+                int index = stack.pop();
+
+                map.put(index, i - index);
+            }
+
+            stack.push(i);
+        }
+
+        int[] ans = new int[temperatures.length];
+
+        for (int i = 0; i < temperatures.length; i++) 
+        {
+            if (map.containsKey(i))
+                ans[i] = map.get(i);
+            else
+                ans[i] = 0;
+        }
+
+        return ans;
+    }
+}
